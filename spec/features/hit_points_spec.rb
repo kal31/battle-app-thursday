dd934e5cb9364ab98bb69957575d59751ceba0cf
amin-newcastle <muhammad-karim@hotmail.com>
2021-10-06 16:39:39 +0100

    Viewing hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,12 +1,10 @@
-feature 'Enter names' do
-    scenario 'submitting names' do
+feature 'Veiw hit points' do
+    scenario 'see Player 2 hit points' do
         visit('/')
         fill_in :player_1_name, with: 'Khalid'
         fill_in :player_2_name, with: 'Amin'
         click_button 'Submit'
-
-        save_and_open_page
-
-        expect(page).to have_content 'Khalid vs. Amin'
+        
+        expect(page).to have_content 'Amin: 100HP'
     end
 end
