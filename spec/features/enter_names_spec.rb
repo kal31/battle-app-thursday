f1796afd9b11c2e9e9cc48a8e7d0f8abf03233b9
khalid <khalidmiah@hotmail.co.uk>
2021-10-07 13:49:39 +0100

    attacking player 2

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,6 +1,10 @@
 feature 'Enter names' do
     scenario 'submitting names' do
         sign_in_and_play
+        # fill_in :player_1_name, with: 'Khalid'
+        # fill_in :player_2_name, with: 'Amin'
+        # click_button 'Submit'
+        # save_and_open_page # will save the web page and open the browser to display it
         expect(page).to have_content 'Khalid vs. Amin'
     end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,7 +2,18 @@ feature 'Veiw hit points' do
     scenario 'see Player 2 hit points' do
         sign_in_and_play
         
-        expect(page).to have_content 'Amin: 100HP'
+        expect(page).to have_content 'Khalid: HP:100'
     end
+
+end
+
+feature 'attack' do
+    scenario 'attack player 2' do
+        sign_in_and_play
+        click_button 'Attack'
+        
+        expect(page).to have_content 'Khalid attacked Amin'
+    end
+
 end
 
