35ade140cb2097aa0f925f2ac5a2e9602a15372f
khalid <khalidmiah@hotmail.co.uk>
2021-10-08 11:51:49 +0100

    reducing HP by 10

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,8 +1,9 @@
+require_relative '../../lib/player.rb'
 feature 'Veiw hit points' do
     scenario 'see Player 2 hit points' do
         sign_in_and_play
         
-        expect(page).to have_content 'Khalid: HP:100'
+        expect(page).to have_content 'Amin: HP:100'
     end
 
 end
@@ -12,7 +13,14 @@ feature 'attack' do
         sign_in_and_play
         click_button 'Attack'
         
-        expect(page).to have_content 'Khalid attacked Amin'
+        expect(page).to have_content "Khalid attacked Amin"
+    end
+
+    scenario 'reduce player 2 hit points by 10' do
+        sign_in_and_play
+        click_button 'Attack'
+        expect(page).not_to have_content 'Amin: HP:100'
+        expect(page).to have_content "Amin: HP:90"
     end
 
 end
--- spec/features/units/player_spec.rb
+++ spec/features/units/player_spec.rb
@@ -2,10 +2,34 @@ require 'player'
 
 describe Player do
     subject(:khalid) { Player.new('Khalid') }
+    subject(:amin) { Player.new('Amin') }
   
     describe '#name' do
       it 'returns the name' do
         expect(khalid.name).to eq 'Khalid'
       end
+    end   
+
+  
+      
+    describe '#hit_points' do
+      it 'returns the hit points' do
+      expect(amin.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+      end
+    end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(amin).to receive(:receive_damage)
+      khalid.attack(amin)
     end
   end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { amin.receive_damage }.to change { amin.hit_points }.by(-10)
+    end
+  end
+
+
+end
