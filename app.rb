886ac3ddecc36ded64e809825d858e4575c02e74
khalid <khalidmiah@hotmail.co.uk>
2021-10-09 12:40:52 +0100

    attack player 2 and confirm

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 require_relative './lib/player.rb'
 
 class Battle < Sinatra::Base
+  enable :sessions
   
     
 
@@ -15,23 +16,25 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-        $player_1 = Player.new(params[:player_1_name]) 
-        $player_2 = Player.new(params[:player_2_name])
-        redirect '/play'
-      end
+      session[:player_1_name] = params[:player_1_name]
+      session[:player_2_name] = params[:player_2_name]
+      redirect '/play'
+    end   
+
+    get '/play' do
+      @player_1_name = session[:player_1_name]
+      @player_2_name = session[:player_2_name]
+      erb :play
+    end
+
+    get '/attack' do
+      @player_1_name = session[:player_1_name]
+      @player_2_name = session[:player_2_name]
+      erb :attack
+    end
+
+    
       
-      get '/play' do
-        @player_1_name = $player_1.name
-        @player_2_name = $player_2.name
-        erb :play
-      end
-      
-      get '/attack' do
-        @player_1_name = $player_1.name
-        @player_2_name = $player_2.name
-        @player_1_name.attack(@player_2_name)
-        erb :attack
-      end  
     
   
     # start the server if ruby file executed directly
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play
     visit('/')
-        fill_in :player_1_name, with: 'Khalid'
-        fill_in :player_2_name, with: 'Amin'
+        fill_in :player_1_name, with: 'Charlotte'
+        fill_in :player_2_name, with: 'Mittens'
         click_button 'Submit'
 end     
